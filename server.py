464ca80c782759361b783a7ed6e29783dff6f3b8
Tin-Tin <kt-chin@hotmail.com>
2015-10-07 14:49:55 +0100

    Played with server

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
-
+﻿
 # code reduced from https://wiki.python.org/moin/BaseHttpServer
 
 import time
 import BaseHTTPServer
+import json
 
 # example of a python class
  
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_GET(s):
+
     """Respond to a GET request."""
     s.send_response(200)
     s.send_header("Content-type", "text/html")
     s.end_headers()
-    s.wfile.write("<html><head><title>Title goes here.</title></head>")
+    s.wfile.write("<html><head><title>My first python program!</title></head>")
     s.wfile.write("<body><p>This is a test.</p>")
+    s.wfile.write("<p>Hello World!</p>")
     s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+    split1 = s.path.split('/', 1)
+    split2 = split1[1].split('+', 1)
+    x = int(split2[0]) + int(split2[1])
+    s.wfile.write(x)
     s.wfile.write("</body></html>")
 
 
